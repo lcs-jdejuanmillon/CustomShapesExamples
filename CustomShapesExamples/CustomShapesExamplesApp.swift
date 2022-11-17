bb391598cf439105e2d39f2b17705e9cb846f97b
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-11-17 11:41:57 -0500

    Initial Commit

---
+++ CustomShapesExamples/CustomShapesExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapesExamplesApp.swift
+//  CustomShapesExamples
+//
+//  Created by Jacobo de Juan Millon on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapesExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
