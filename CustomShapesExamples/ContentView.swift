1f3e09ed2bb3cad331b5cab1389903c6762b0e2e
Jacobo de Juan <jdejuanmillon@lcs.on.ca>
2022-11-17 12:34:30 -0500

    Added Diamond and WeirdShapeThing

--- CustomShapesExamples/ContentView.swift
+++ CustomShapesExamples/ContentView.swift
@@ -22,10 +22,50 @@ struct Diagonal: Shape {
     }
 }
 
+struct Diamond: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // Here is where we'd describe the shape...
+        path.move(to: CGPoint(x: rect.midX, y: 0))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.midX, y: 0))
+        // Send back (return) the path that describes the shape
+        return path
+    }
+}
+
+struct WeirdShapeThing: Shape {
+    var sectionsDivided: Int
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        for i in 1..<sectionsDivided {
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: rect.maxX, y: Double(i) * rect.maxY / Double(sectionsDivided)))
+        }
+        for i in 1..<sectionsDivided {
+            path.move(to: CGPoint(x: rect.maxX, y: 0))
+            path.addLine(to: CGPoint(x: Double(i) * rect.maxX / Double(sectionsDivided), y: rect.maxY))
+        }
+        for i in 1..<sectionsDivided {
+            path.move(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: 0, y: Double(i) * rect.maxY / Double(sectionsDivided)))
+        }
+        for i in 1..<sectionsDivided {
+            path.move(to: CGPoint(x: 0, y: rect.maxY))
+            path.addLine(to: CGPoint(x: Double(i) * rect.maxX / Double(sectionsDivided), y: 0))
+        }
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Diagonal()
+        Diamond()
             .stroke()
+            
     }
 }
 
